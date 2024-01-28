3033c3ddda7359786b471cf6dd767fe6465716f4
d.shaikhullin <d.shaikhullin@mail.business.ru>
2024-01-28 23:17:45 +0500

    added clipRRect and set infinity size to image

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -2,55 +2,44 @@ import 'package:flutter/material.dart';
 
 class CustomImage extends StatelessWidget {
   final String imageUrl;
+  final double radius;
 
   const CustomImage({
     Key? key,
     required this.imageUrl,
+    this.radius = 20,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      imageUrl,
-      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-        if (loadingProgress == null) {
-          return child;
-        } else {
-          return Center(
-            child: CircularProgressIndicator(
-              value: loadingProgress.expectedTotalBytes != null
-                ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-                : null,
-            ),
-          );
-        }
-      },
-      errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-        return Container(
-          alignment: Alignment.center,
-          child: Image.asset(
-            'assets/images/placeholder.png',
-          )
-        );
-      },
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: Image.network(
+          imageUrl,
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
+          loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+            if (loadingProgress == null) {
+              return child;
+            } else {
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                    : null,
+                ),
+              );
+            }
+          },
+          errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+            return Image.asset(
+              'assets/images/placeholder.png',
+            );
+          },
+        ),
+      ),
     );
-    // return FadeInImage(
-    //   placeholder: Image.asset(
-    //     alignment: Alignment.center,
-    //     fit: BoxFit.scaleDown,
-    //      'assets/images/placeholder.png'
-    //   ).image,
-    //   imageErrorBuilder: (context, error, stackTrace) {
-    //     return Container(
-    //       alignment: Alignment.center,
-    //       child: Image.asset(
-    //         'assets/images/placeholder.png',
-    //       ),
-    //     );
-    //   },
-    //   image: Image.network(
-    //     imageUrl,
-    //   ).image,
-    // );
   }
 }
