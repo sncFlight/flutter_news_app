885d3ef16e14256a6b43f3dc355ad33e454bb528
d.shaikhullin <d.shaikhullin@mail.business.ru>
2024-01-28 23:18:19 +0500

    added new TextStyles

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TextStyles {
-  static TextStyle description({
-    Color? color,
+  static TextStyle notation({
+    Color? color = Colors.grey,
     double fontSize = 12,
   }) {
-    return GoogleFonts.inter(
+    return GoogleFonts.notoSans(
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle header({
+    Color? color = Colors.grey,
+    double fontSize = 16,
+  }) {
+    return GoogleFonts.notoSans(
+      fontSize: fontSize,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle common({
+    Color? color = Colors.white,
+    double fontSize = 12,
+  }) {
+    return GoogleFonts.notoSans(
       fontSize: fontSize,
       color: color,
     );
