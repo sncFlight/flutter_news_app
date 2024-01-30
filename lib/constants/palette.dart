dec5280d217828776efd9389b098802d7b943ff4
d.shaikhullin <d.shaikhullin@mail.business.ru>
2024-01-31 02:47:34 +0500

    added colors

--- lib/constants/palette.dart
+++ lib/constants/palette.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static const Color appBackgroundLight = Color(0xFF1E1E1E);
-  static const Color appBackgroundDark = Color(0xFF1E1E1E);
-  static const Color articlesBackground = Color.fromARGB(255, 46, 6, 6);
+  static const Color appBackground = Colors.white;
+  static Color articleReadTile = Colors.grey.shade200;
+  static Color shimmer = Colors.grey.shade300;
+  static Color articleNotReadTile = Colors.grey.shade500;
+  static Color appBarBackground = Colors.blue.withOpacity(0.5);
 }
